3128573fb33688a50ba8b5c5c2645271a4b0973b
icook <icook@LAPTOP-NCUV50HH>
2025-05-05 18:47:04 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Assignment_5._1._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+            int b = 20;
+            Sum(a, b);
+            Console.WriteLine(Sum(a, b));
+        }
+
+        public static int Sum(int x, int y)
+        {
+            return x + y;
+        }
+    }
+}
